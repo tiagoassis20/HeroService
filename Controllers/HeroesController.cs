c5b1a83ad82c047279dd2304d0f69d3ce1baa78d
tiagoassis20 <tiagoassis20@gmail.com>
2017-08-08 23:36:28 -0300

    Initial commit

---
+++ Controllers/HeroesController.cs
@@ -0,0 +1,40 @@
+﻿using HeroService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace HeroService.Controllers
+{
+    public class HeroesController : ApiController
+    {
+        Hero[] heroes = new Hero[]
+        {
+            new Hero () { Id =  0,  Name =  "Zero" },
+            new Hero () { Id =  11, Name =  "Mr. Nice" },
+            new Hero () { Id =  12, Name =  "Narco" },
+            new Hero () { Id =  13, Name =  "Bombasto" },
+            new Hero () { Id =  14, Name =  "Celeritas" },
+            new Hero () { Id =  15, Name =  "Magneta" },
+            new Hero () { Id =  16, Name =  "RubberMan" },
+            new Hero () { Id =  17, Name =  "Dynama" },
+            new Hero () { Id =  18, Name =  "Dr IQ" },
+            new Hero () { Id =  19, Name =  "Magma" },
+            new Hero () { Id =  20, Name =  "Tornado" }
+        };
+
+        public IEnumerable<Hero> GetHeroes()
+        {
+            return heroes;
+        }
+        public IHttpActionResult GetHero(int id)
+        {
+            var hero = heroes.FirstOrDefault(p => p.Id == id);
+            if (hero == null)
+                return NotFound();
+            return Ok(hero);
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HeroService.WebApiApplication" Language="C#" %>
